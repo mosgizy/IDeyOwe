d4054f512ee9027efd2e57f9ca0890d77f50597c
mosgizy <moshood521@gmail.com>
2023-12-26 08:25:14 +0100

    set sameSite on session for google auth

--- app.js
+++ app.js
@@ -32,7 +32,9 @@ app.use(require('express-session')(
   {
     secret: process.env.GOOGLE_CLIENT_SECRET,
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
+    sameSite: 'None',
+    secure:true
   }));
 app.use(passport.initialize());
 app.use(passport.session());
