757d179b21716c4430293fd1674d7160d3138d1b
mosgizy <moshood521@gmail.com>
2023-12-27 08:55:01 +0100

    fix allow origin for google auth

--- routes/Google.js
+++ routes/Google.js
@@ -8,8 +8,6 @@ router.route('/').get(passport.authenticate('google', { scope: ['profile', 'emai
 router.route('/callback').get(passport.authenticate('google', { failureRedirect: '/' }),
   async (req, res) => {
     const { user } = req
-    
-    // console.log(user)
 
     let userAuth = await Auth.findOne({ email: user.email })
     
@@ -24,13 +22,13 @@ router.route('/callback').get(passport.authenticate('google', { failureRedirect:
 
     const token = userAuth.createJWT()
 
-    res.cookie('token', token, { maxAge: 3600000, httpOnly: true });
+    res.cookie('token', token, { maxAge: 3600000, httpOnly: true,sameSite: 'None', secure: true });
     // res.status(201).json({token:userAuth.createJWT(),user:{name:userAuth.name,Email:userAuth.email}})
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', process.env.REMOTE_BASE_URL);
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.redirect('/api/v1/dept');
+    res.redirect(process.env.REMOTE_BASE_URL);
   }
 );
 
